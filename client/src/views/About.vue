1353da301b247ac7e067aa902dc0232c872b8423
sysKG <suyanqi924@gmail.com>
2022-05-11 20:55:50 +0800

    add replication package link into about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,9 +1,43 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-      
+    <a href="https://github.com/sysKG/BugFinding" target="_blank">Replication Package</a>
+
+    <!-- <h1>About</h1> -->
+      <!-- <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="num" label="No." width="200" />
+        <el-table-column prop=“bugid” label="BugID" width=600>
+            <template v-slot="props">
+              <a :href="props.row.bugid" target="_blank">{{props.row.bugid}}</a>          
+            </template>
+        </el-table-column>
+        <el-table-column prop="period" label="Period" />
+      </el-table>   -->
   </div>
 </template>
+
+<script>
+export default {
+    name:'about',
+    props:[],
+    components:{
+    },
+    data() {
+    
+    return {
+      // tableData: [
+      //               {
+      //                 num: '1',
+      //                 bugid: 'https://bugzilla.mozilla.org/show_bug.cgi?id=1744772',
+      //                 period: 'tool development',
+      //               },
+                   
+      //             ]
+
+    }
+  },
+}
+</script>
+
 
 <style lang="scss">
   .card {
